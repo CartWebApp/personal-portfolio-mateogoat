75cb52baf9c5d2bc9b9126322c091361b40bd76d
Mateo Siechert <86209620+mateogoat@users.noreply.github.com>
2026-05-04 19:37:54 -0700

    responsive nav bar (somewhat good)

--- script.js
+++ script.js
@@ -29,3 +29,11 @@ function animateCircles() {
         y += (nextCircle.y - y) * 0.15;
     });
 }
+
+const navbarToggle = document.querySelector('.navbar-toggle');
+const navbarMenu = document.querySelector('.navbar-menu');
+
+navbarToggle.addEventListener('click', () => {
+    navbarToggle.classList.toggle('active');
+    navbarMenu.classList.toggle('active');
+})
